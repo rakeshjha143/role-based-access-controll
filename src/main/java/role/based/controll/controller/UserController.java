8ebf59a44aa378ffbed0fbbe06feb4afb344cb1a
rakesh1518 <rakesh1518@outlook.com>
2020-08-23 17:37:05 +0530

    change user edit page

--- src/main/java/role/based/controll/controller/UserController.java
+++ src/main/java/role/based/controll/controller/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-
+import role.based.controll.entity.Role;
 import role.based.controll.entity.User;
 import role.based.controll.services.RolesServices;
 import role.based.controll.services.UserServices;
@@ -49,5 +51,19 @@ public class UserController {
 	     userServices.saveUser(user);
 	      return "redirect:";
 	   }
+	 
+	  @RequestMapping("/edituser/{user_id}")
+	  public ModelAndView showEditUser(@PathVariable(name = "user_id") Long user_id) {
+	      ModelAndView mav = new ModelAndView("user/edit_user");
+	      User user = userServices.findByroleId(user_id);
+	      mav.addObject("user", user);
+	       
+	      return mav;
+	  }
+	  @RequestMapping("/deleteuser/{user_id}")
+	  public String deleteUser(@PathVariable(name = "user_id") Long user_id) {
+		  userServices.deleteUser(user_id);
+	      return "redirect:/";       
+	  }
 	
 }
